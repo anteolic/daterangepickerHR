9bff75b8b071a9f6367d69a5808d93448a208b09
anteolic <ante.olic@gmail.com>
2024-04-03 10:57:06 +0200

    Update date-range-filter.js

--- date-range-filter.js
+++ date-range-filter.js
@@ -358,7 +358,7 @@ const run = async (
         }
         ${!def_range_obj ? `autoUpdateInput: false,` : ""}
         locale: {
-          format: 'dd.mm.yyyy',
+          format: 'd.m.Y',
           ${!ranges?.length ? `cancelLabel: "${extra.req.__("Clear")}"` : ""}
         },
         ${set_initial}
@@ -371,7 +371,7 @@ const run = async (
             ? `$('#daterangefilter${name}').on('apply.daterangepicker', function(ev, picker) {
           console.log("pick")
 
-          $(this).val(picker.startDate.format('dd.mm.yyyy') + ' - ' + picker.endDate.format('dd.mm.yyyy'));
+          $(this).val(picker.startDate.format('d.m.Y') + ' - ' + picker.endDate.format('d.m.Y'));
         });
     
         $('#daterangefilter${name}').on('cancel.daterangepicker', function(ev, picker) {
